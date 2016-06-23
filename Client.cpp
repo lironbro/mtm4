8417631d8eecff86fa4f2484938cb77628ec820a
lironbro <liron.bronfman@gmail.com>
2016-06-23 13:57:13 +0300

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,37 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: 21 αιεπι 2016
+ *      Author: Liron
+ */
+
+#include "Client.h"
+
+
+Client::Client(int priority, BrokerIfc& broker,
+		std::ostream& messagesSink):	id(allocId),
+		priority(priority), broker(broker), messagesSink(messagesSink),
+		topics(SortedSet<Topic, CompareTopics>()){
+	if(priority < 0)
+		throw IllegalPriority();
+	allocId++;
+}
+
+
+int Client::getPriority() const{
+	return this->priority;
+}
+
+
+int Client::getId() const{
+	return this->id;
+}
+
+
+void Client::receiveMaintenanceMessage(const std::string& s) const{
+	messagesSink << "Client #"<< this->id <<
+			" received maintenance message: "<< s << std::endl;
+}
+
+
+
